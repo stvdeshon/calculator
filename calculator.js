d0e2aa2cccd1146c1f71848f33b5a9a83da3b88a
stvdeshon <stvdeshon@gmail.com>
2022-03-27 11:44:08 -0600

    Improve the input functionality

--- calculator.js
+++ calculator.js
@@ -1,25 +1,24 @@
 const display = document.querySelector('#display');
 const numbers = document.querySelectorAll('.face');
-const operators = document.querySelectorAll('.operators');
+const operators = document.querySelectorAll('.operator');
 const clear = document.querySelector('#clear');
 const del = document.querySelector('#del');
 const negToggle = document.querySelector('#negative-toggle');
 const decimal = document.querySelector('#decimal');
-const add = document.querySelector('#add');
-const subtract = document.querySelector('#subtract');
-const multiply = document.querySelector('#multiply');
-const divide = document.querySelector('#divide');
-const equal = document.querySelector('#equal');
-const one = document.querySelector('#one');
-const two = document.querySelector('#two');
-const three = document.querySelector('#three');
-const four = document.querySelector('#four');
-const five = document.querySelector('#five');
-const six = document.querySelector('#six');
-const seven = document.querySelector('#seven');
-const eight = document.querySelector('#eight');
-const nine = document.querySelector('#nine');
-let dot = false;
+// const add = document.querySelector('#add');
+// const subtract = document.querySelector('#subtract');
+// const multiply = document.querySelector('#multiply');
+// const divide = document.querySelector('#divide');
+// const equal = document.querySelector('#equal');
+// const one = document.querySelector('#one');
+// const two = document.querySelector('#two');
+// const three = document.querySelector('#three');
+// const four = document.querySelector('#four');
+// const five = document.querySelector('#five');
+// const six = document.querySelector('#six');
+// const seven = document.querySelector('#seven');
+// const eight = document.querySelector('#eight');
+// const nine = document.querySelector('#nine');
 
 
 function addition(num1, num2){
@@ -45,46 +44,61 @@ function operate(operator, num1, num2){
     if(operator === '/') return division(num1, num2);
 }
 
+let dot = false;
+
 //the number string for the display
-number = '';
+let number = '';
 
 //currently adds numbers to the display
 numbers.forEach(function(item) {
+    item.addEventListener('click', function(e) {    
+        number += e.target.textContent;
+        display.textContent = number;
+  });
+})
+
+operators.forEach(function(item) {
     item.addEventListener('click', function(e) {
         number += e.target.textContent;
         display.textContent = number;
-    });
-});
-
-//the variable that holds the operator strings
-symbol = '';
-
-
-//Right now these don't operate on the numbers
-operators.forEach(function(item) {
-    item.addEventListener('click', function(e) {
-        symbol = e.target.textContent;
-        display.textContent = symbol;
     })
 })
 
-//the following three functions display the decimal and toggle it 
-function decimalStop(){
-    dot = true;
-}
+//the following two lines appear completely useless, for now
+// if(dot) document.getElementById('decimal').disabled = true;
+// if(!dot) document.getElementById('decimal').disabled = false;
+
+// dot = false; must also come from the operators
 
 decimal.addEventListener('click', function(e) {
     if(!dot){
-    display.textContent += e.target.textContent;
+    number += e.target.textContent;
+    display.textContent = number;
     }
 })
+
+function decimalStop(){
+    if(number.includes('.')){
+    dot = true;
+    }else if(!number.includes('.')){
+        dot = false;
+    }
+}
+
 decimal.addEventListener('click', decimalStop);
 
+//deletes the latest input
+del.addEventListener('click', function(){
+    number = number.slice(0, -1);
+    display.textContent = number;
+})
+console.log(number);
 //clears the display and returns toggles to default
 function erase(){
     display.innerHTML = '';
     number = ''
     dot = false;
+    if(!dot) document.getElementById('decimal').disabled = false;
 }
 
 clear.addEventListener('click', erase);
