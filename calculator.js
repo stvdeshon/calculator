372648f334c56bdca85a5706824c197ac104dd68
stvdeshon <stvdeshon@gmail.com>
2022-03-28 10:46:09 -0600

    Add some calculation functionality, but needs work

--- calculator.js
+++ calculator.js
@@ -1,148 +1,137 @@
 const display = document.querySelector('#display');
-const numbers = document.querySelectorAll('.face');
+const numpad = document.querySelectorAll('.numpad');
 const operators = document.querySelectorAll('.operator');
 const clear = document.querySelector('#clear');
 const del = document.querySelector('#del');
 const negToggle = document.querySelector('#negative-toggle');
 const decimal = document.querySelector('#decimal');
-const add = document.querySelector('#add');
-const subtract = document.querySelector('#subtract');
-const multiply = document.querySelector('#multiply');
 const divide = document.querySelector('#divide');
-const equal = document.querySelector('#equal');
-//the number string for the display
-let number = '';
-let inputOne = '';
-let inputTwo = '';
-let operatorSymbol = '';
+const multiply = document.querySelector('#multiply');
+const subtract = document.querySelector('#subtract');
+const add = document.querySelector('#add');
 let dot = false;
+let operandOne = '';
+let operandTwo = '';
+let operator = '';
 
-//find a way to call operator start
-
-function addition(num1, num2){
-    return number = num1 + num2;
-}
-
-function subtraction(num1, num2){
-    return number = num1 - num2;
-}
-
-function multiplication(num1, num2){
-    return number = num1 * num2;
-}
-
-function division(num1, num2){
-    return number = num1 / num2;
-}
-
-function operate(operator, num1, num2){
-    num1 = Number(num1);
-    num2 = Number(num2);
-    if(operator === '+') return addition(num1, num2);
-    if(operator === '-') return subtraction(num1, num2);
-    if(operator === '*') return multiplication(num1, num2);
-    if(operator === '/') return division(num1, num2);
-
-    
-}
-
-// function callOperate(){
-//     return operate(operatorSymbol, inputOne, inputTwo);
-// }
+//still need to update to display to UI properly
 
 
-
-
-//currently adds numbers to the display
-numbers.forEach(function(item) {
+numpad.forEach(function(item) {
     item.addEventListener('click', function(e) {    
-        number += e.target.textContent;
-        display.textContent = number;
+        // number += e.target.textContent;
+        // display.textContent = number;
+
+        //tries to prevent early use of operator to prevent bugs
+        if(operandOne === '' && operator.length > 0) operator = '';
+
+        //helps solve division by 0;
+        if(operandOne === undefined) operandOne = '';
+
+        //attempts to only allow input to operandOne on the condition the operator is empty
+        if(operator.length === 0) {
+            operandOne += e.target.textContent;
+        }else if(operator.length > 0) operandTwo += e.target.textContent;
   });
-  console.log(operate());
 })
 
 operators.forEach(function(item) {
     item.addEventListener('click', function(e) {
-        operatorSymbol = e.target.textContent;
-        if(inputOne.length === 0 || inputTwo.length >= 1) {
-            inputOne = number;
-            inputTwo = '';
+        // number += e.target.textContent;
+        // display.textContent = number;
+        operator = e.target.textContent;
+
+        //helps solve division by 0
+        if(operandOne === undefined) return;
+
+        //tries to prevent early use of operator to prevent bugs
+        if(operandOne.length === 0 && operator.length > 0) operator = '';
+
+        //calls operate(), stores its return value in operandOne, and refreshes operandTwo
+        //can add and subtract fine, can only multiply and divide first then breaks
+        if(operandTwo.length > 0) {
+            operandTwo = '';
+            // operandOne = operate(operator, operandOne, operandTwo);
         }
-        else if(inputOne.length >= 1 || inputTwo.length === 0){ 
-            inputTwo = number;
-            inputOne = '';
-        } 
-        number = '';
-        display.textContent = number;        
-
-        dot = false;
-        console.log(operatorSymbol);
-        console.log(operate());
-        //put number into input1, if input1 has something, put number into input2, delete number both times
-        
-        if(inputTwo === '') return operate(operatorSymbol, inputOne, inputTwo);
-
     })
 })
 
-equal.addEventListener('click', operate);
-equal.addEventListener('click', function(){
-    // if(inputOne.length === 0 || inputTwo.length >= 1) {
-    //     inputOne = number;
-    //     inputTwo = '';
-    // }
-    // else if(inputOne.length >= 1 || inputTwo.length === 0){ 
-    //     inputTwo = number;
-    //     inputOne = '';
-    // } 
-    console.log(operate(operatorSymbol, inputOne, inputTwo));
-    console.log('Im number' + number);
-    console.log('im input one' + inputOne);
-    console.log('im input two' + inputTwo);
-    console.log(operatorSymbol);
-    })
 
 
-    // saved in case new version breaks
-// operators.forEach(function(item) {
-//     item.addEventListener('click', function(e) {
-//         number += e.target.textContent;
-//         display.textContent = number;
-//         operate(operatorSymbol, number, numberTwo);
-//         dot = false;
-//         console.log(number);
-//     })
+
+equal.addEventListener('click', function(e) {
+    console.log(`hi im ${operandOne} and im ${operator} and im${operandTwo}`);
+    // console.log(operate(operator, operandOne, operandTwo));
+    operandOne = operate(operator, operandOne, operandTwo);
+
+})
+
+// decimal.addEventListener('click', function(e) {
+//     // if(!dot){
+//     // number += e.target.textContent;
+//     // display.textContent = number;
+//     // }
+//     if(operator.length === 0 && !operandOne.includes('.')) {
+//         operandOne += e.target.textContent;
+//     }else if(operator.length > 0 && !operandTwo.includes('.')) operandTwo += e.target.textContent;
 // })
 
-decimal.addEventListener('click', function(e) {
-    if(!dot){
-    number += e.target.textContent;
-    display.textContent = number;
-    }
-})
-
 function decimalStop(){
-    if(number.includes('.')) dot = true;
-    if(!number.includes('.')) dot = false;
-}
+    // if(number.includes('.')){
+    // dot = true;
+    // }else if(!number.includes('.')){
+    //     dot = false;
+    // }
+    //attempts to only allow input to operandOne on the condition the operator is empty
+    
 
+}
 decimal.addEventListener('click', decimalStop);
 
-//deletes the latest input
+//quasi-functional delete mechanic 
 del.addEventListener('click', function(){
-    if(number.length > 0) number = number.slice(0, -1);
-    display.textContent = number;
+    // display.textContent = displayvalue;
+    console.log(`hi im ${operandOne} and im ${operator} and im${operandTwo}`);
+    if(operator.length === 0) {
+        operandOne = operandOne.slice(0, 1);
+    }else if(operator.length > 0) operandTwo = operandTwo.slice(0, -1);
 })
 
-//clears the display and returns toggles to default
+//restores everything to default
 function erase(){
     display.innerHTML = '';
-    number = ''
-    inputOne = '';
-    inputTwo = '';
+    operandOne = '';
+    operandTwo = '';
+    operator = '';
     dot = false;
     if(!dot) document.getElementById('decimal').disabled = false;
 }
 
 clear.addEventListener('click', erase);
+
+
+function addition(num1, num2){
+    return num1 + num2;
+}
+
+function subtraction(num1, num2){
+    return num1 - num2;
+}
+
+function multiplication(num1, num2){
+    return num1 * num2;
+}
+
+function division(num1, num2){
+    return num1 / num2;
+}
+
+function operate(operation, num1, num2){
+    num1 = Number(num1);
+    num2 = Number(num2);
+    if(num2 === 0) return undefined;
+    if(operation === '+') return addition(num1, num2);
+    if(operation === '-') return subtraction(num1, num2);
+    if(operation === '*') return multiplication(num1, num2);
+    if(operation === '/') return division(num1, num2);
+}
